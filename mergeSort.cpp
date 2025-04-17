f35beefbd4e1f3fbd5f8b55f25871e36035b85e2
tari2025 <denilestari272@gmail.com>
2025-04-17 10:55:41 +0700

    input

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array: ";
+        cin >> n;
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n===========================";
+    cout << "\nInputkan Isi Element Array";
+    cout << "\n=============================";
+    endl;
+
+    for (int i = 0
